05527954b72a362e3d42106b6111cd5596f7b7ff
Clément TARIEL <clement.tariel@gmail.com>
2021-12-31 18:59:30 +0100

    projet partie omp

--- Projet/sources/README.md
+++ Projet/sources/README.md
@@ -26,7 +26,7 @@ Les différentes étapes du projets ont été réalisées dans différents progr
 
 ## Résultats
 
-### Mesure du temps
+### Mesure du temps (simulation.cpp)
 
 
 Temps de calcul par étape sans affichage | Temps d'affichage par étape | Total | speedup
@@ -35,34 +35,54 @@ Temps de calcul par étape sans affichage | Temps d'affichage par étape | Total
 
 On constate qu'avec un programme séquentiel, à chaque étape, l'affichage prend plus de 40% du temps de calcul.
 
-### Parallélisation affichage contre simulation
+### Parallélisation affichage contre simulation (simulation_sync_affiche_mpi.cpp)
 
 Temps de calcul par étape sans affichage | Temps d'affichage par étape | speedup
 -----------------------------------------|-----------------------------|---------
                  0.029                   |             0.029           |  1.62
 
 Avec une séparation du calcul et de l'affichage sur 2 processus parrallèle mais s'envoyant leur donnée de manière synchrone, le temps de calcul est à peine plus long qu'en séquentiel (le temps d'envoyer le tableau de données au processus qui gère l'affichage).
-Cependant le temps de calcul et le temps d'affichage ne se somment plus puisqu'ils sont gérés sur des coeurs différents et on obtient ainsi un gain de temps de plus de 30%.
+Le temps de calcul et le temps d'affichage ne se somment plus puisqu'ils sont gérés sur des coeurs différents et on obtient ainsi un gain de temps de plus de 30%.
 On peut tout de même noter que quand il y a une différence de vitesse d'exécution entre les 2 processus, l'utilisation d'envois synchrone pénalise le plus rapide.
 
 
-### Parallélisation affichage asynchrone contre simulation
+### Parallélisation affichage asynchrone contre simulation (simulation_async_affiche_mpi.cpp)
 
 Temps de calcul par étape sans affichage | Temps d'affichage par étape | speedup
 -----------------------------------------|-----------------------------|---------
-                 0.024                   |             0.050           |  1.96
+                 0.025                   |             0.050           |  1.88
 
 En n'envoyant les données calculées au processus afficheur que lorsque celui est disponible pour les recevoir, on évite de faire perdre du temps au processus calculeur.
 En effet avec la methode précédente, le processus calculeur doit se synchroniser avec le processus afficheur, le temps que ce dernier récupère les données de l'étape antérieure et les affiche. En étant asynchrone, quand il y a un décalage, c'est le proccessus afficheur qui attend et on rend ainsi le processus calculeur plus rapide.
 L'inconvenient est que l'on sollicite presque 2 fois moins le processus afficheur, ce qui fait baisser la fréquence de d'affichage, mais cela n'est pas perceptible à l'oeil nu.
 
 
-### Parallélisation OpenMP
+### Parallélisation OpenMP (simulation_async_omp.cpp)
+
+On remarque que la génération aléatoire implémentée dans individu.cpp dépend de l'ordre dans lequel les individus sont traités. Or OpenMP ne permet pas de gérer l'ordre dans lequel les individus sont traités, on ne peut donc pas garantir un résultat identique à celui obtenu pour une exécution séquentielle.
+
+Pour un même nombre d'individu on a :
+
+OMP_NUM_THREADS | Temps de calcul par étape sans affichage | Temps d'affichage par étape | speedup
+----------------|------------------------------------------|-----------------------------|---------
+      1         |                  0.027                   |             0.055           |  1.74
+      2         |                  0.018                   |             0.055           |  2.61
+      3         |                  0.019                   |             0.061           |  2.47
+      4         |                  0.018                   |             0.056           |  2.61
+
+Pour un même nombre d'individu proportionellement au nombre de thread on a :
+
+OMP_NUM_THREADS | Temps de calcul par étape sans affichage | Temps d'affichage par étape | speedup
+----------------|------------------------------------------|-----------------------------|---------
+      1         |                  0.027                   |             0.055           |  1.74
+      2         |                  0.037                   |             0.11            |  1.27
+      3         |                  0.058                   |             0.18            |  0.81
+      4         |                  0.068                   |             0.23            |  0.69
 
 
-### Parallélisation MPI de la simulation
+### Parallélisation MPI de la simulation (simulation_async_mpi.cpp)
 
 
-
+### bilan ?
 
 
--- Projet/sources/simulation_async_omp.cpp
+++ Projet/sources/simulation_async_omp.cpp
@@ -7,9 +7,14 @@
 #include "graphisme/src/SDL2/sdl2.hpp"
 
 #include <chrono>
+# include <mpi.h>
+
+
+
 
 void màjStatistique( épidémie::Grille& grille, std::vector<épidémie::Individu> const& individus )
 {
+    # pragma omp parallel for
     for ( auto& statistique : grille.getStatistiques() )
     {
         statistique.nombre_contaminant_grippé_et_contaminé_par_agent = 0;
@@ -18,6 +23,7 @@ void màjStatistique( épidémie::Grille& grille, std::vector<épidémie::Indivi
     }
     auto [largeur,hauteur] = grille.dimension();
     auto& statistiques = grille.getStatistiques();
+    # pragma omp parallel for
     for ( auto const& personne : individus )
     {
         auto pos = personne.position();
@@ -77,11 +83,34 @@ void afficheSimulation(sdl2::window& écran, épidémie::Grille const& grille, s
     écran << sdl2::flush;
 }
 
-void simulation(bool affiche)
+void simulation(bool affiche, int rank,int nbp)
 {
 
+
+    MPI_Status status ;
+
+    MPI_Status quit_status ;
+
+    MPI_Request quit_request ;
+
+    int ready_to_receive_flag = 0;
+
+    bool ready_to_receive;
+
+    //MPI_Request jours_request ;
+
+    //MPI_Request grille_request ;    
+
+
+    int nb_iteration = 0;
+
     constexpr const unsigned int largeur_écran = 1280, hauteur_écran = 1024;
+    
     sdl2::window écran("Simulation épidémie de grippe", {largeur_écran,hauteur_écran});
+    if (rank != 0){
+        sdl2::finalize();
+    }
+
 
     unsigned int graine_aléatoire = 1;
     std::uniform_real_distribution<double> porteur_pathogène(0.,1.);
@@ -89,7 +118,7 @@ void simulation(bool affiche)
 
     épidémie::ContexteGlobal contexte;
     // contexte.déplacement_maximal = 1; <= Si on veut moins de brassage
-    // contexte.taux_population = 400'000;
+    contexte.taux_population = 400'000;
     //contexte.taux_population = 1'000;
     contexte.interactions.β = 60.;
     std::vector<épidémie::Individu> population;
@@ -125,88 +154,179 @@ void simulation(bool affiche)
 
 
     std::cout << "Début boucle épidémie" << std::endl << std::flush;
+
+    std::chrono::time_point<std::chrono::system_clock> start, middle, end;
+
+    bool zero_quit = false;
+    int flag;
+    if (rank >0){
+        MPI_Irecv(&zero_quit,1,MPI_C_BOOL,0,1,MPI_COMM_WORLD,& quit_request );
+        //std::cout << "rank "<<rank<<" quitting = false" << std::endl;
+            
+    }
+
+    //temp
+    std::vector<épidémie::Grille::StatistiqueParCase> temp_grid;
+    temp_grid.reserve(contexte.taux_population);
+    temp_grid = grille.getStatistiques();
+
+    //MPI_Barrier(MPI_COMM_WORLD);
+    
     while (!quitting)
     {
-        std::chrono::time_point<std::chrono::system_clock> start, middle, end;
         start = std::chrono::system_clock::now();
+        nb_iteration ++;
 
-        auto events = queue.pull_events();
-        for ( const auto& e : events)
-        {
-            if (e->kind_of_event() == sdl2::event::quit)
-                quitting = true;
+        if (rank == 0){
+            auto events = queue.pull_events();
+            for ( const auto& e : events)
+            {
+                if (e->kind_of_event() == sdl2::event::quit){
+                    quitting = true;
+                    zero_quit = true;
+                    for (int k=1;k<nbp;k++){
+                        MPI_Isend(&zero_quit, 1, MPI_C_BOOL, k, 1, MPI_COMM_WORLD,& quit_request ) ;
+                        //std::cout << "rank 0 to rank "<<k<<" send quitting message" << std::endl;
+            
+                    }
+                }
+            }
+            //reception des donnees
+            //std::cout << "rank "<<rank<<" trying to receive message" << std::endl;
+            //MPI_Irecv(&jours_écoulés,1,MPI_INT,MPI_ANY_SOURCE,0,MPI_COMM_WORLD,& jours_request );
+            if (!quitting){
+                MPI_Send(&ready_to_receive, 1, MPI_INT, 1, 4, MPI_COMM_WORLD);
+                
+                MPI_Recv(&jours_écoulés , 1, MPI_INT, MPI_ANY_SOURCE, 2, MPI_COMM_WORLD ,& status );
+                
+                //MPI_Irecv(temp_grid.data(), 3*largeur_grille*hauteur_grille, MPI_INT, MPI_ANY_SOURCE,0,MPI_COMM_WORLD,& grille_request );
+                MPI_Recv(temp_grid.data(), 3*largeur_grille*hauteur_grille, MPI_INT, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD ,& status );
+                //std::cout << "1 : "<< sizeof(temp_grid) <<","<< sizeof(grille.getStatistiques()) << std::endl;
+                grille.set_m_statistiques(temp_grid);
+                //std::cout << "2 : "<< sizeof(temp_grid) <<","<< sizeof(grille.getStatistiques()) << std::endl;
+                afficheSimulation(écran, grille, jours_écoulés);
+                //std::cout << "rank "<<rank<<" message received" << std::endl;
+            }
+            
         }
-        if (jours_écoulés%365 == 0)// Si le premier Octobre (début de l'année pour l'épidémie ;-) )
-        {
-            grippe = épidémie::Grippe(jours_écoulés/365);
-            jour_apparition_grippe = grippe.dateCalculImportationGrippe();
-            grippe.calculNouveauTauxTransmission();
-            // 23% des gens sont immunisés. On prend les 23% premiers
-            for ( int ipersonne = 0; ipersonne < nombre_immunisés_grippe; ++ipersonne)
-            {
-                population[ipersonne].devientImmuniséGrippe();
-            }
-            for ( int ipersonne = nombre_immunisés_grippe; ipersonne < int(contexte.taux_population); ++ipersonne )
-            {
-                population[ipersonne].redevientSensibleGrippe();
-            }
-        }
-        if (jours_écoulés%365 == std::size_t(jour_apparition_grippe))
-        {
-            for (int ipersonne = nombre_immunisés_grippe; ipersonne < nombre_immunisés_grippe + 25; ++ipersonne )
-            {
-                population[ipersonne].estContaminé(grippe);
-            }
-        }
-        // Mise à jour des statistiques pour les cases de la grille :
-        màjStatistique(grille, population);
-        // On parcout la population pour voir qui est contaminé et qui ne l'est pas, d'abord pour la grippe puis pour l'agent pathogène
-        std::size_t compteur_grippe = 0, compteur_agent = 0, mouru = 0;
-        for ( auto& personne : population )
-        {
-            if (personne.testContaminationGrippe(grille, contexte.interactions, grippe, agent))
-            {
-                compteur_grippe ++;
-                personne.estContaminé(grippe);
-            }
-            if (personne.testContaminationAgent(grille, agent))
-            {
-                compteur_agent ++;
-                personne.estContaminé(agent);
-            }
-            // On vérifie si il n'y a pas de personne qui veillissent de veillesse et on génère une nouvelle personne si c'est le cas.
-            if (personne.doitMourir())
-            {
-                mouru++;
-                unsigned nouvelle_graine = jours_écoulés + personne.position().x*personne.position().y;
-                personne = épidémie::Individu(nouvelle_graine, contexte.espérance_de_vie, contexte.déplacement_maximal);
-                personne.setPosition(largeur_grille, hauteur_grille);
-            }
-            personne.veillirDUnJour();
-            personne.seDéplace(grille);
-        }
-        //blabla temps elapsed
-        middle = std::chrono::system_clock::now();
-        std::chrono::duration<double> elapsed_seconds = middle-start;
-        std::cout << "Temps calcul par etape (sans affichage) : " << elapsed_seconds.count() 
-              << std::endl;
+        if(rank > 0){
+            
+            
 
+            
+            if (jours_écoulés%365 == 0)// Si le premier Octobre (début de l'année pour l'épidémie ;-) )
+            {
+                grippe = épidémie::Grippe(jours_écoulés/365);
+                jour_apparition_grippe = grippe.dateCalculImportationGrippe();
+                grippe.calculNouveauTauxTransmission();
+                // 23% des gens sont immunisés. On prend les 23% premiers
+                for ( int ipersonne = 0; ipersonne < nombre_immunisés_grippe; ++ipersonne)
+                {
+                    population[ipersonne].devientImmuniséGrippe();
+                }
+                for ( int ipersonne = nombre_immunisés_grippe; ipersonne < int(contexte.taux_population); ++ipersonne )
+                {
+                    population[ipersonne].redevientSensibleGrippe();
+                }
+            }
+            if (jours_écoulés%365 == std::size_t(jour_apparition_grippe))
+            {
+                for (int ipersonne = nombre_immunisés_grippe; ipersonne < nombre_immunisés_grippe + 25; ++ipersonne )
+                {
+                    population[ipersonne].estContaminé(grippe);
+                }
+            }
+            // Mise à jour des statistiques pour les cases de la grille :
+            màjStatistique(grille, population);
+            // On parcout la population pour voir qui est contaminé et qui ne l'est pas, d'abord pour la grippe puis pour l'agent pathogène
+            std::size_t compteur_grippe = 0, compteur_agent = 0, mouru = 0;
+            # pragma omp parallel for reduction(+:compteur_grippe,compteur_agent,mouru) 
+            for ( auto& personne : population )
+            {   
+                
+                if (personne.testContaminationGrippe(grille, contexte.interactions, grippe, agent))
+                {
+                    compteur_grippe ++;
+                    personne.estContaminé(grippe);
+                }
+                if (personne.testContaminationAgent(grille, agent))
+                {
+                    compteur_agent ++;
+                    personne.estContaminé(agent);
+                }
+                // On vérifie si il n'y a pas de personne qui veillissent de veillesse et on génère une nouvelle personne si c'est le cas.
+                if (personne.doitMourir())
+                {
+                    mouru++;
+                    unsigned nouvelle_graine = jours_écoulés + personne.position().x*personne.position().y;
+                    personne = épidémie::Individu(nouvelle_graine, contexte.espérance_de_vie, contexte.déplacement_maximal);
+                    personne.setPosition(largeur_grille, hauteur_grille);
+                }
+                personne.veillirDUnJour();
+                personne.seDéplace(grille);
+            }
+            //blabla temps elapsed
+            /*
+            middle = std::chrono::system_clock::now();
+            std::chrono::duration<double> elapsed_seconds = middle-start;
+            std::cout << "rank "<<rank<<". Temps calcul par etape (sans affichage) : " << elapsed_seconds.count() 
+                  << std::endl;//*/
+
+            //envoi des donnees
+            //std::cout << "rank "<<rank<<" trying to send message" << std::endl;
+            /*int MPI_Iprobe(
+              int source,
+              int tag,
+              MPI_Comm comm,
+              int *flag,
+              MPI_Status *status
+            );*/
+            MPI_Iprobe( MPI_ANY_SOURCE, 4, MPI_COMM_WORLD, &ready_to_receive_flag,  MPI_STATUS_IGNORE );
+            if (ready_to_receive_flag){
+                MPI_Recv(&ready_to_receive , 1, MPI_INT, MPI_ANY_SOURCE, 4, MPI_COMM_WORLD ,& status );
+            
+                //MPI_Isend(&jours_écoulés, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,& jours_request ) ;
+                MPI_Send(&jours_écoulés, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+                
+
+                temp_grid = grille.getStatistiques();
+                //std::cout << "3 : "<< sizeof(temp_grid) <<","<< sizeof(grille.getStatistiques()) << std::endl;
+                
+                //std::cout << sizeof(temp_grid) <<","<< contexte.taux_population <<","<< 3*largeur_grille*hauteur_grille << std::endl;
+                //MPI_Isend(temp_grid.data(), 3*largeur_grille*hauteur_grille, MPI_INT, 0, 0, MPI_COMM_WORLD,& grille_request ) ;
+                MPI_Send(temp_grid.data(), 3*largeur_grille*hauteur_grille, MPI_INT, 0, 3, MPI_COMM_WORLD);
+                //std::cout << "rank "<<rank<<" message sent" << std::endl;
+            }
+
+            MPI_Test(&quit_request, &flag,&quit_status) ;
+            if (flag!=0){
+                //received
+                quitting = true;// = zero_quit
+                //std::cout << "rank "<<rank<<" quitting = true" << std::endl;
+            
+            }
+        }
         //#############################################################################################################
         //##    Affichage des résultats pour le temps  actuel
         //#############################################################################################################
-        if (affiche) afficheSimulation(écran, grille, jours_écoulés);
-
+        /*if (affiche) {
+            afficheSimulation(écran, grille, jours_écoulés);
+        }*/
         /*std::cout << jours_écoulés << "\t" << grille.nombreTotalContaminésGrippe() << "\t"
                   << grille.nombreTotalContaminésAgentPathogène() << std::endl;*/
-
-        end = std::chrono::system_clock::now();
-        std::chrono::duration<double> elapsed_seconds2 = end-middle;
-        std::cout << "Temps calcul par etape (avec affichage) : " << elapsed_seconds.count() + elapsed_seconds2.count() 
-              << std::endl;
-        std::cout << "Temps calcul par etape pour l'affichage : " << elapsed_seconds2.count() 
-              << std::endl;
-
-
+        if (rank == 0){
+            end = std::chrono::system_clock::now();
+            std::chrono::duration<double> elapsed_seconds2 = end-start;
+            std::cout << "rank "<<rank<<", iteration "<<nb_iteration<<". Temps calcul par etape (recuperation + affichage ): " << elapsed_seconds2.count() 
+                  << std::endl;
+        }else{
+            end = std::chrono::system_clock::now();
+            std::chrono::duration<double> elapsed_seconds2 = end-start;
+            std::cout << "rank "<<rank<<", iteration "<<nb_iteration<<". Temps calcul par etape (calcul + envoi ): " << elapsed_seconds2.count() 
+                  << std::endl;
+        }
+        
+        
         output << jours_écoulés << "\t" << grille.nombreTotalContaminésGrippe() << "\t"
                << grille.nombreTotalContaminésAgentPathogène() << std::endl;
         jours_écoulés += 1;
@@ -219,17 +339,51 @@ void simulation(bool affiche)
 
 int main(int argc, char* argv[])
 {
+    // On initialise le contexte MPI qui va s'occuper :
+    //    1. Créer un communicateur global, COMM_WORLD qui permet de gérer
+    //       et assurer la cohésion de l'ensemble des processus créés par MPI;
+    //    2. d'attribuer à chaque processus un identifiant ( entier ) unique pour
+    //       le communicateur COMM_WORLD
+    //    3. etc...
+    MPI_Init( &argc, &argv );
+    // Pour des raisons de portabilité qui débordent largement du cadre
+    // de ce cours, on préfère toujours cloner le communicateur global
+    // MPI_COMM_WORLD qui gère l'ensemble des processus lancés par MPI.
+    MPI_Comm globComm;
+    MPI_Comm_dup(MPI_COMM_WORLD, &globComm);
+    // On interroge le communicateur global pour connaître le nombre de processus
+    // qui ont été lancés par l'utilisateur :
+    int nbp;
+    MPI_Comm_size(globComm, &nbp);
+    // On interroge le communicateur global pour connaître l'identifiant qui
+    // m'a été attribué ( en tant que processus ). Cet identifiant est compris
+    // entre 0 et nbp-1 ( nbp étant le nombre de processus qui ont été lancés par
+    // l'utilisateur )
+    int rank;
+    MPI_Comm_rank(globComm, &rank);
+
     // parse command-line
     bool affiche = true;
     for (int i=0; i<argc; i++) {
       std::cout << i << " " << argv[i] << "\n";
       if (std::string("-nw") == argv[i]) affiche = false;
     }
-  
     sdl2::init(argc, argv);
     {
-        simulation(affiche);
+        simulation(affiche,rank,nbp);
     }
     sdl2::finalize();
+
+
+    //std::cout << "rank "<<rank<<" reach barrier" << std::endl;
+            
+    MPI_Barrier(MPI_COMM_WORLD);  
+    // A la fin du programme, on doit synchroniser une dernière fois tous les processus
+    // afin qu'aucun processus ne se termine pendant que d'autres processus continue à
+    // tourner. Si on oublie cet instruction, on aura une plantage assuré des processus
+    // qui ne seront pas encore terminés.
+    //std::cout << "rank "<<rank<<" finalize" << std::endl;
+    MPI_Finalize();
+
     return EXIT_SUCCESS;
 }
